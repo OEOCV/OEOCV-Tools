06d833fec9ead2e2728467d01549130cb30828f1
OEOCV_Creator <contact@oeocv.org>
2021-05-18 02:50:47 -0500

    Added the ability to iterate through a directory.

--- source/remove_strongs/remove_strongs.py
+++ source/remove_strongs/remove_strongs.py
@@ -1,10 +1,49 @@
-import re
+""" Strong's Remover
+Removes references to Strong's Concordance from USFM files in order to
+keep the text consistent with the Septuagint version
+and to simplify the editing process.
+"""
 
-def main():
-	book = '\v 1 \w The|strong="G3588"\w* \w elder|strong="G4245"\w* \w to|strong="G1722"\w* \w Gaius|strong="G1050"\w* \w the|strong="G3588"\w* \w beloved|strong="G27"\w*, \w whom|strong="G3739"\w* \w I|strong="G1473"\w* \w love|strong="G25"\w* \w in|strong="G1722"\w* \w truth|strong="G225"\w*.'
+import os
+import re
+import sys
+
+
+def remove_text(book):
 	new_book = re.sub(r'\b(\|strong)\b[^"]+"([^"]*)"', "", book)  # Uses a raw string for the regex pattern
 	new_book = re.sub(r'(\\w\*|\\w )', "", new_book)
-	print(new_book)
+	return new_book
+
+def main():
+	if len(sys.argv) == 1:
+		print("Error: Enter the path to either a file or a directory")
+		sys.exit()
+	current_path = sys.argv[1]
+	if os.path.isdir(current_path):
+		print("Directory")
+		os.chdir(current_path)
+		for current_book in os.listdir():
+			if os.path.splitext(current_book)[-1] != ".usfm":  # Only works for USFM files
+				continue
+			file = open(current_book, 'r+')
+			text = file.read()
+			new_text = remove_text(text)
+			file.seek(0)
+			file.truncate(0)
+			file.write(new_text)
+			file.close()
+	else:
+		print("File")
+		if os.path.splitext(current_path)[-1] != ".usfm":  # Only works for USFM files
+			print("Error: Only compatible with USFM files")
+			sys.exit()
+		file = open(current_path, 'r+')
+		text = file.read()
+		new_text = remove_text(text)
+		file.seek(0)
+		file.truncate(0)
+		file.write(new_text)
+		file.close()
 
 if __name__ == "__main__":
 	main()
