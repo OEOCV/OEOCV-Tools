eff7eee68107b6574d8cdb424b90af48385ab4b6
OEOCV_Creator <contact@oeocv.org>
2021-05-19 18:40:47 -0500

    Added JSON parsing

--- source/validate/validate.sh
+++ source/validate/validate.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Requires usfm-grammar (https://github.com/Bridgeconn/usfm-grammar) to be installed globally already
+# Requires jq and usfm-grammar (https://github.com/Bridgeconn/usfm-grammar) to be installed globally already
 
 # Assumes both git repositories are in the same directory. Modify if necessary
 BIBLE_PATH=../../../OEOCV/source
 
 for current_book in $BIBLE_PATH/*.usfm; do
 		[ -e "$current_book" ] || continue
-		usfm-grammar $current_book # TODO: Show only the error messages to make this easier to read
+		JSON_OUT=$(usfm-grammar $current_book) # TODO: Show only the error messages to make this easier to read
+		#echo $JSON_OUT
+		JSON_PARSED=$(echo $JSON_OUT | jq -M '.. | ._warnings? //empty')
+		echo $JSON_PARSED
 done
