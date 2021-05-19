55f2e3a838b4a2ff92cbf7d25dc555c0834486cb
OEOCV_Creator <contact@oeocv.org>
2021-05-19 15:15:15 -0500

    Made the script check all files.

--- source/validate/validate.sh
+++ source/validate/validate.sh
@@ -4,3 +4,8 @@
 
 # Assumes both git repositories are in the same directory. Modify if necessary
 BIBLE_PATH=../../../OEOCV/source
+
+for current_book in $BIBLE_PATH/*.usfm; do
+		[ -e "$current_book" ] || continue
+		usfm-grammar $current_book # TODO: Show only the error messages to make this easier to read
+done
