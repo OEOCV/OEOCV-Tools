04ecd707e2e001388a831cfccd9963a4ed593a75
OEOCV_Creator <contact@oeocv.org>
2021-05-19 14:59:56 -0500

    Started writing a validator script

---
+++ source/validate/validate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Requires usfm-grammar (https://github.com/Bridgeconn/usfm-grammar) to be installed globally already
+
+# Assumes both git repositories are in the same directory. Modify if necessary
+BIBLE_PATH=../../../OEOCV/source
