62abf499c010f4dce1f7a6a9be7a68a1affb4c98
OEOCV_Creator <contact@oeocv.org>
2021-05-19 19:27:05 -0500

    Removed comment

--- source/validate/validate.sh
+++ source/validate/validate.sh
@@ -7,7 +7,7 @@ BIBLE_PATH=../../../OEOCV/source
 
 for current_book in $BIBLE_PATH/*.usfm; do
 		[ -e "$current_book" ] || continue
-		JSON_OUT=$(usfm-grammar $current_book) # TODO: Show only the error messages to make this easier to read
+		JSON_OUT=$(usfm-grammar $current_book)
 		#echo $JSON_OUT
 		JSON_PARSED=$(echo $JSON_OUT | jq -M '.. | ._warnings? //empty')
 		if [ "$JSON_PARSED" != '[]' ] && [ ! -z "$JSON_PARSED" ]
